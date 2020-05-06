13ef9584281171b866c250e8b5fff2858170917b
Omkommersind <chauzov@list.ru>
2020-05-06 16:53:33 +0300

    Initial

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float MoveSpeed = 5f;
+    public float MouseSensitivity = 1f;
+    public Transform CameraTransform;
+
+    private Rigidbody _rb;
+
+    private Vector3 _moveInput;
+    private Vector3 _mouseInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Movement
+        _moveInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        Vector3 moveX = transform.right * _moveInput.x;
+        Vector3 moveZ = transform.forward * _moveInput.z;
+
+        _rb.velocity = (moveX + moveZ) * MoveSpeed;
+
+        // View
+        _mouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")) * MouseSensitivity;
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + _mouseInput.x,
+            transform.rotation.eulerAngles.z);
+
+        CameraTransform.localRotation = Quaternion.Euler(CameraTransform.localRotation.eulerAngles + new Vector3(-_mouseInput.y, 0, 0));
+    }
+}
